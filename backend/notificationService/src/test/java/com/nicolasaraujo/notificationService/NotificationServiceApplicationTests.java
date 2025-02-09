ff1a11ab21410f246913a2a3a1126b0b74ace982
NicolasFCAraujo <nicolasfcaraujo@gmail.com>
2025-02-09 16:38:09 -0300

    notification service created

---
+++ backend/notificationService/src/test/java/com/nicolasaraujo/notificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasaraujo.notificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
