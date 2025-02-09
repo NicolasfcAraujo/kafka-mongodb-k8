d087ea3578c97f5ae3910e4c3938d1c7818362f8
NicolasFCAraujo <nicolasfcaraujo@gmail.com>
2025-02-09 19:34:48 -0300

    findAllNotificationsByReceiverId function created

--- backend/notificationService/src/main/java/com/nicolasaraujo/notificationService/repository/NotificationRepository.java
+++ backend/notificationService/src/main/java/com/nicolasaraujo/notificationService/repository/NotificationRepository.java
@@ -3,5 +3,9 @@ package com.nicolasaraujo.notificationService.repository;
 import com.nicolasaraujo.notificationService.model.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface NotificationRepository extends MongoRepository<Notification, String> {
+
+    List<Notification> findAllByReceiverId(String id);
 }
