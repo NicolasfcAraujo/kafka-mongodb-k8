9e3ecd7770bbc8b0ab69b7e27bd58553efd4c03c
NicolasFCAraujo <nicolasfcaraujo@gmail.com>
2025-02-09 16:35:53 -0300

    application service created

---
+++ backend/applicationService/src/test/java/com/nicolasaraujo/applicationService/ApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasaraujo.applicationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
